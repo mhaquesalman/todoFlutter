7ecb301d89283a2251478407bdb04baf78497cc6
mhaquesalman <mhaquesalman@gmail.com>
2021-08-27 00:01:30 +0600

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todoflutter/todo_list_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
